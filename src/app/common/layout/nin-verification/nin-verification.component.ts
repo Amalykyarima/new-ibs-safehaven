b3f20f44e85e1fd3c82d8822921fceee27b3cf6b
Ameh <david.ameh37@gmail.com>
2025-06-01 22:19:49 +0100

    dev2 first push

---
+++ src/app/common/layout/nin-verification/nin-verification.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { SelectBoxComponent } from "../../utilities/select-box/select-box.component";
+import { InputComponent } from "../../utilities/input/input.component";
+import { OtpInputComponent } from "../../utilities/otp-input/otp-input.component";
+import { ButtonFilledComponent } from "../../utilities/button-filled/button-filled.component";
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nin-verification',
+  standalone: true,
+  imports: [SelectBoxComponent, InputComponent, OtpInputComponent, ButtonFilledComponent, CommonModule],
+  templateUrl: './nin-verification.component.html',
+  styleUrl: './nin-verification.component.scss'
+})
+export class NinVerificationComponent {
+  verificationMethods: any = [
+    { name: 'SMS', icon: '../../../../assets/icons/identity-sms.svg', activeIcon: '../../../../assets/icons/identity-sms-active.svg' },
+    { name: 'Face', icon: '../../../../assets/icons/identity-face.svg', activeIcon: '../../../../assets/icons/identity-face-active.svg' },
+  ];
+  loading: boolean = false
+  activeMethod: string = '';
+
+  setVerificationMethod(value: string) {
+    this.activeMethod = value;
+  }
+  verifyBVN() {
+    
+  }
+}
--- src/app/common/utilities/otp-input/otp-input.component.ts
+++ src/app/common/utilities/otp-input/otp-input.component.ts
@@ -21,9 +21,31 @@ import { NgOtpInputModule } from 'ng-otp-input';
   templateUrl: './otp-input.component.html',
   styleUrl: './otp-input.component.scss'
 })
-export class OtpInputComponent {
+export class OtpInputComponent implements OnInit, OnChanges {
+  @Input() length: number = 6;
+  @Input() error = '';
+  @Output() onChange: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() {
+  pinConfig: any = {};
+
+  ngOnInit(): void {
+    this.initializePinConfig();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['error']) {
+      if (this.error !== '') {
+        this.initializePinConfig(true);
+        setTimeout(() => {
+          this.error = '';
+        }, 1000);
+      } else {
+        this.initializePinConfig(false);
+      }
+    }
+  }
+
+  initializePinConfig(hasError: boolean = false) {
     this.pinConfig = {
       length: this.length,
       disableAutoFocus: true,
@@ -31,59 +53,17 @@ export class OtpInputComponent {
       isPasswordInput: true,
       inputClass: 'border-none',
       containerClass:
-        'flex items-center sm:gap-[20px] gap-[14px] justify-between  w-full',
+        'flex items-center sm:gap-[20px] gap-[14px] justify-between w-full',
       inputStyles: {
         'min-height': '52px',
-        border: 'none',
         'min-width': '42px',
         background: '#F4F5F7',
         'border-radius': '10px',
-        color: '#034EA2',
+        color: hasError ? 'red' : '#034EA2',
+        border: hasError ? '1px solid red' : 'none',
       },
     };
   }
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['error'] && changes['error'].currentValue !== '') {
-      this.pinConfig = {
-        length: this.length,
-        disableAutoFocus: true,
-        allowNumbersOnly: true,
-        isPasswordInput: true,
-        inputClass: 'border-none',
-        containerClass: 'flex items-center gap-[20px] justify-between  w-full',
-        inputStyles: {
-          'min-height': '52px',
-          'min-width': '42px',
-          border: '1px solid red',
-          background: '#F4F5F7',
-          'border-radius': '10px',
-          color: 'red',
-        },
-      };
-      setTimeout(() => {
-        this.error = '';
-      }, 1000);
-    } else if (changes['error'] && changes['error'].currentValue === '') {
-      this.pinConfig = {
-        length: this.length,
-        disableAutoFocus: true,
-        allowNumbersOnly: true,
-        isPasswordInput: true,
-        inputClass: 'border-none',
-        containerClass: 'flex items-center gap-[20px] justify-between  w-full',
-        inputStyles: {
-          'min-height': '52px',
-          border: 'none',
-          'min-width': '42px',
-          background: '#F4F5F7',
-          'border-radius': '10px',
-          color: '#034EA2',
-        },
-      };
-    }
-  }
-
-  ngOnInit(): void {}
 
   ngAfterViewInit() {
     const otpInputs = document.querySelectorAll('.otp-input');
@@ -92,17 +72,10 @@ export class OtpInputComponent {
     });
   }
 
-  @Input() length: number = 6;
-  @Input() error = '';
-  @Output() onChange: EventEmitter<any> = new EventEmitter<any>();
-
-  pinConfig: any = {};
-
   onPINChange = (event: any) => {
     if (event.length === this.length) {
       this.pinConfig.inputStyles.color = '#034EA2';
       this.pinConfig.inputStyles.border = 'none';
-      // //(event);
       this.onChange.emit(event);
     }
   };
@@ -115,12 +88,114 @@ export class OtpInputComponent {
     ) as HTMLInputElement[];
 
     let val = pastedData.slice(0, this.length);
-
-    let l = val.length >= this.length ? this.length : val.length;
-    for (let i = 0; i < l; i++) {
+    for (let i = 0; i < val.length; i++) {
       otpInputs[i].value = val[i];
     }
     event.preventDefault();
     this.onChange.emit(val);
   }
 }
+
+// export class OtpInputComponent {
+//   @Input() length: number = 6;
+//   @Input() error = '';
+//   @Output() onChange: EventEmitter<any> = new EventEmitter<any>();
+  
+//   constructor() {
+//     this.pinConfig = {
+//       length: this.length,
+//       disableAutoFocus: true,
+//       allowNumbersOnly: true,
+//       isPasswordInput: true,
+//       inputClass: 'border-none',
+//       containerClass:
+//         'flex items-center sm:gap-[20px] gap-[14px] justify-between w-full',
+//       inputStyles: {
+//         'min-height': '52px',
+//         border: 'none',
+//         'min-width': '42px',
+//         background: '#F4F5F7',
+//         'border-radius': '10px',
+//         color: '#034EA2',
+//       },
+//     };
+//   }
+//   ngOnChanges(changes: SimpleChanges): void {
+//     if (changes['error'] && changes['error'].currentValue !== '') {
+//       this.pinConfig = {
+//         length: this.length,
+//         disableAutoFocus: true,
+//         allowNumbersOnly: true,
+//         isPasswordInput: true,
+//         inputClass: 'border-none',
+//         containerClass: 'flex items-center gap-[20px] justify-between  w-full',
+//         inputStyles: {
+//           'min-height': '52px',
+//           'min-width': '42px',
+//           border: '1px solid red',
+//           background: '#F4F5F7',
+//           'border-radius': '10px',
+//           color: 'red',
+//         },
+//       };
+//       setTimeout(() => {
+//         this.error = '';
+//       }, 1000);
+//     } else if (changes['error'] && changes['error'].currentValue === '') {
+//       this.pinConfig = {
+//         length: this.length,
+//         disableAutoFocus: true,
+//         allowNumbersOnly: true,
+//         isPasswordInput: true,
+//         inputClass: 'border-none',
+//         containerClass: 'flex items-center gap-[20px] justify-between  w-full',
+//         inputStyles: {
+//           'min-height': '52px',
+//           border: 'none',
+//           'min-width': '42px',
+//           background: '#F4F5F7',
+//           'border-radius': '10px',
+//           color: '#034EA2',
+//         },
+//       };
+//     }
+//   }
+
+//   ngOnInit(): void {}
+
+//   ngAfterViewInit() {
+//     const otpInputs = document.querySelectorAll('.otp-input');
+//     otpInputs.forEach((input) => {
+//       input.addEventListener('paste', (e) => this.handlePaste(e));
+//     });
+//   }
+
+
+//   pinConfig: any = {};
+
+//   onPINChange = (event: any) => {
+//     if (event.length === this.length) {
+//       this.pinConfig.inputStyles.color = '#034EA2';
+//       this.pinConfig.inputStyles.border = 'none';
+//       // //(event);
+//       this.onChange.emit(event);
+//     }
+//   };
+
+//   handlePaste(event: any) {
+//     const clipboardData = event?.clipboardData;
+//     const pastedData = clipboardData?.getData('text');
+//     const otpInputs = Array.from(
+//       document.querySelectorAll('.otp-input')
+//     ) as HTMLInputElement[];
+
+//     let val = pastedData.slice(0, this.length);
+
+//     let l = val.length >= this.length ? this.length : val.length;
+//     for (let i = 0; i < l; i++) {
+//       otpInputs[i].value = val[i];
+//     }
+//     event.preventDefault();
+//     this.onChange.emit(val);
+//   }
+// }
